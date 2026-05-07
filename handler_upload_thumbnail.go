ac032001188df58c431d51abe9819f8088bacc84
mike_the_sloth <mjooste13@gmail.com>
2026-05-07 20:15:09 +0200

    multiple thumbnail methods

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -45,11 +49,13 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	// `file` is an `io.Reader` that we can read from to get the image data
 	mediaType := header.Header.Get("Content-Type")
-	fileData, err := io.ReadAll(file)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Unable to read form file", err)
-		return
-	}
+	/*
+		fileData, err := io.ReadAll(file)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Unable to read form file", err)
+			return
+		}
+	*/
 	databaseVideo, err := cfg.db.GetVideo(videoID)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Unable to get database video metadata", err)
@@ -59,12 +65,35 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		respondWithError(w, http.StatusUnauthorized, "User not owner of video", err)
 		return
 	}
-	thumbnailStruct := thumbnail{
-		data:      fileData,
-		mediaType: mediaType,
+	/*
+		thumbnailStruct := thumbnail{
+			data:      fileData,
+			mediaType: mediaType,
+		}
+	*/
+	//videoThumbnails[videoID] = thumbnailStruct
+	//thumbnailURL := fmt.Sprintf("http://`localhost:8091/api/thumbnails/%s", videoIDString)
+	//encodedData := base64.StdEncoding.EncodeToString(fileData)
+	mimeMediaType, _, err := mime.ParseMediaType(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get mime media type for file", err)
+		return
 	}
-	videoThumbnails[videoID] = thumbnailStruct
-	thumbnailURL := fmt.Sprintf("http://localhost:8091/api/thumbnails/%s", videoIDString)
+	if mimeMediaType != "image/jpeg" && mimeMediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Wrong image type", nil)
+		return
+	}
+	extension := strings.Split(mediaType, "/")[1]
+	FileLocationEnd := fmt.Sprintf("/assets/%s.%s", videoIDString, extension)
+	FilePath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", videoIDString, extension))
+	createdFile, err := os.Create(FilePath)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get create file", err)
+		return
+	}
+	io.Copy(createdFile, file)
+	//thumbnailURL := fmt.Sprintf("data:%s;base64,%s", mediaType, encodedData)
+	thumbnailURL := fmt.Sprintf("http://localhost:8091%s", FileLocationEnd)
 	databaseVideo.ThumbnailURL = &thumbnailURL
 	err = cfg.db.UpdateVideo(databaseVideo)
 	respondWithJSON(w, http.StatusOK, databaseVideo)
